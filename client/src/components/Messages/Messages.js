07141cc6d133c8d963481a0d9a69f73f9bd1688c
nilesh-05 <nilesh.s0506@gmail.com>
2021-02-04 19:38:11 +0530

    Completed

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -4,8 +4,10 @@ import './Messages.css'
 import Message from '../Message/Message'
 
 const Messages = ({ messages, name }) => {
-	<ScrollToBottom>
-		{messages.map((message, i) => <div key={i}><Message message={message} name={name} /></div>)}
-	</ScrollToBottom>
+	return (
+		<ScrollToBottom className='messages'>
+			{messages.map((message, i) => <div key={i}><Message message={message} name={name} /></div>)}
+		</ScrollToBottom>
+	)
 }
 export default Messages
